83ae068c6650af8872a3797b9ec59eea45b876fd
Raju Gottumukkala <Raju Gottumukkala>
2016-09-15 23:35:43 +0800

    With updated styling

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var About = (props) => {
   return (
-    <h3>About</h3>
+    <h1 className="text-center page-title">About</h1>
   )
 };
 
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -29,7 +29,7 @@ render: function() {
         <form onSubmit={this.onSearch}>
           <ul className="menu">
             <li>
-              <input type="search" placeholder="Search Weather"/>
+              <input type="search" placeholder="Search Weather by city"/>
             </li>
             <li>
               <input type="submit" className="button" value="Get Weather"/>
